0d531284500aa386fa2b000127b2b5e03045129f
Jorge Barahona <bacj010509@gs.utm.mx>
2024-01-13 13:27:50 -0600

    Servidor.

--- cloud_server.py
+++ cloud_server.py
@@ -1,18 +1,34 @@
 import threading
 import zmq
 
-def cloud_server(node_id, port):
+def cloud_server(node_id, host, port):
+    """
+    Función para manejar el servidor de la nube.
+
+    Args:
+        node_id (int): Identificador del nodo.
+        host (str): Dirección IP del host.
+        port (int): Puerto de comunicación.
+
+    """
+    # Inicializar el contexto y el socket REP (Reply) de ZeroMQ
     context = zmq.Context()
     socket = context.socket(zmq.REP)
-    socket.bind(f"tcp://127.0.0.1:{port}")
+    socket.bind(f"tcp://{host}:{port}")
 
+    # Bucle principal del servidor
     while True:
+        # Recibir mensaje del cliente
         message = socket.recv_string()
-        print(f"Nodo {node_id} reibido: {message}")
+        print(f"Nodo {node_id} recibido: {message}")
+
+        # Enviar respuesta al cliente
         response = f"Respuesta del nodo {node_id}"
         socket.send_string(response)
 
-if __name__ == "__main__":
-    node_thread = threading.Thread(target=cloud_server, args=(1, 5555))
-    node_thread.start()
-    node_thread.join()
+# Iniciar hilo del servidor
+node_thread = threading.Thread(target=cloud_server, args=(1, "192.168.3.6", 5555))
+node_thread.start()
+
+# Esperar a que el hilo del servidor termine (en este caso, nunca sucede debido al bucle infinito)
+node_thread.join()
