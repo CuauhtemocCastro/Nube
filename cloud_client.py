f6e53710dd2749fdfb992c7ec34c7da46076383d
Jorge Barahona <bacj010509@gs.utm.mx>
2024-01-13 15:13:27 -0600

    Servidor.

--- cloud_client.py
+++ cloud_client.py
@@ -1,18 +1,57 @@
+# VERSION 2
+
 import socket
+import os
 
-def cloud_client():
-    # Crea un socket TCP/IP
+def receive_file(connection, filename):
+    with open(filename, 'wb') as file:
+        while True:
+            data = connection.recv(1024)
+            if not data:
+                break
+            file.write(data)
+
+def cloud_server():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(("192.168.3.6", 8000))
+    sock.listen(5)
 
-    # Conecta el socket en el puerto cuando el servidor esté escuchando (8000)
-    sock.connect(("192.168.3.6", 8000))
+    print("Servidor escuchando en 192.168.3.6:8000")
 
-    message = input("Mensaje a enviar: ")
+    while True:
+        connection, address = sock.accept()
+        print(f"Conexión establecida desde {address}")
 
-    # Escribe los datos al servidor
-    sock.sendall(message.encode())
+        # Recibe el nombre del archivo
+        filename = connection.recv(1024).decode()
+        print(f"Recibiendo archivo: {filename}")
 
-    # Cierra la conexión
-    sock.close()
+        # Recibe el archivo
+        receive_file(connection, filename)
+        print(f"Archivo {filename} recibido correctamente")
 
-cloud_client()
+        # Cierra la conexión
+        connection.close()
+
+cloud_server()
+
+# VERSION 1
+
+# import socket
+
+# def cloud_client():
+#     # Crea un socket TCP/IP
+#     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#     # Conecta el socket en el puerto cuando el servidor esté escuchando (8000)
+#     sock.connect(("192.168.3.6", 8000))
+
+#     message = input("Mensaje a enviar: ")
+
+#     # Escribe los datos al servidor
+#     sock.sendall(message.encode())
+
+#     # Cierra la conexión
+#     sock.close()
+
+# cloud_client()
