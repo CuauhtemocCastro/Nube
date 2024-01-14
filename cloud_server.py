391a8a619f7f98e0673a8bfb69ee65ff128841a5
Jorge Barahona <bacj010509@gs.utm.mx>
2024-01-13 19:23:11 -0600

    Servidor.

--- cloud_server.py
+++ cloud_server.py
@@ -47,10 +47,10 @@ def handle_client(connection, address):
 
 def cloud_server():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind(("192.168.1.6", 8000))
+    sock.bind(("192.168.3.6", 8000))
     sock.listen(5)
 
-    print("Servidor escuchando en 192.168.1.69:8000")
+    print("Servidor escuchando en 192.168.1.6:8000")
 
     while True:
         connection, address = sock.accept()
