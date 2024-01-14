cc3bc4c6f16f85ef1ee96f7a196eb836afc4c0a6
Jorge Barahona <bacj010509@gs.utm.mx>
2024-01-13 19:20:23 -0600

    Version cliente y servidor (Said).

--- cloud_client.py
+++ cloud_client.py
@@ -1,45 +1,44 @@
-# VERSION 2
-
+#cliente
 import socket
 
 def send_file(sock, filename):
     with open(filename, 'rb') as file:
         for data in file:
             sock.sendall(data)
+            print(f"Enviados {len(data)} bytes")
+
+def download_file(sock, filename):
+    destination_path = r'C:\Users\jorge\OneDrive\Documents\Redes' + filename
+    with open(destination_path, 'wb') as file:
+        while True:
+            data = sock.recv(1024)
+            if not data:
+                break
+            file.write(data)
+            print(f"Recibidos {len(data)} bytes")
 
 def cloud_client():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(("192.168.3.6", 8000))
+    sock.connect(("192.168.1.69", 8000))
 
-    # Envia el nombre del archivo
-    filename = input("Ingrese el nombre del archivo: ")
-    sock.sendall(filename.encode())
+    # Solicita al usuario la acción (upload o download)
+    action = input("Ingrese 'upload' para subir un archivo o 'download' para descargar: ")
+    sock.sendall(action.encode())
 
-    # Envia el archivo
-    send_file(sock, filename)
+    if action == 'upload':
+        # El cliente quiere subir un archivo al servidor
+        filename = input("Ingrese el nombre del archivo a subir: ")
+        sock.sendall(filename.encode())
+        send_file(sock, filename)
+        print(f"Archivo {filename} enviado correctamente")
+    elif action == 'download':
+        # El cliente quiere descargar un archivo del servidor
+        filename = input("Ingrese el nombre del archivo a descargar: ")
+        sock.sendall(filename.encode())
+        download_file(sock, filename)
+        print(f"Archivo {filename} descargado correctamente")
 
     # Cierra la conexión
     sock.close()
 
 cloud_client()
-
-# VERSION 1
-
-# import socket
-
-# def cloud_client():
-#     # Crea un socket TCP/IP
-#     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-#     # Conecta el socket en el puerto cuando el servidor esté escuchando (8000)
-#     sock.connect(("192.168.3.6", 8000))
-
-#     message = input("Mensaje a enviar: ")
-
-#     # Escribe los datos al servidor
-#     sock.sendall(message.encode())
-
-#     # Cierra la conexión
-#     sock.close()
-
-# cloud_client()
--- cloud_server.py
+++ cloud_server.py
@@ -1,81 +1,63 @@
-# VERSION 2
+#servidor
 
 import socket
 import os
 import threading
 
+# Establece la ruta de destino para los archivos descargados
+DOWNLOAD_PATH = r'"C:\Users\jorge\OneDrive\Documents\Redes"'
+
 def receive_file(connection, filename):
-    with open(filename, 'wb') as file:
+    destination_path = os.path.join(DOWNLOAD_PATH, filename)
+    with open(destination_path, 'wb') as file:
         while True:
             data = connection.recv(1024)
             if not data:
                 break
             file.write(data)
+            print(f"Recibidos {len(data)} bytes")
+
+def send_file(connection, filename):
+    with open(filename, 'rb') as file:
+        for data in file:
+            connection.sendall(data)
+            print(f"Enviados {len(data)} bytes")
 
 def handle_client(connection, address):
     print(f"Conexión establecida desde {address}")
 
-    # Recibe el nombre del archivo
-    filename = connection.recv(1024).decode()
-    print(f"Recibiendo archivo: {filename}")
+    # Recibe la solicitud del cliente (upload o download)
+    request = connection.recv(1024).decode()
 
-    # Recibe el archivo
-    receive_file(connection, filename)
-    print(f"Archivo {filename} recibido correctamente")
+    if request == 'upload':
+        # El cliente quiere subir un archivo al servidor
+        filename = connection.recv(1024).decode()
+        print(f"Recibiendo archivo: {filename}")
+        receive_file(connection, filename)
+        print(f"Archivo {filename} recibido correctamente")
+    elif request == 'download':
+        # El cliente quiere descargar un archivo del servidor
+        filename = connection.recv(1024).decode()
+        print(f"Enviando archivo: {filename}")
+        send_file(connection, filename)
+        print(f"Archivo {filename} enviado correctamente")
 
     # Cierra la conexión
     connection.close()
 
 def cloud_server():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind(("192.168.3.6", 8000))
+    sock.bind(("192.168.1.69", 8000))
     sock.listen(5)
 
-    print("Servidor escuchando en 192.168.3.6:8000")
+    print("Servidor escuchando en 192.168.1.69:8000")
 
     while True:
         connection, address = sock.accept()
         print(f"Conexión establecida desde {address}")
 
-        # Incia un thread para manejar la conexión con multiples clientes
+        # Inicia un hilo para manejar la conexión con múltiples clientes
         client_thread = threading.Thread(target=handle_client, args=(connection, address))
         client_thread.start()
 
-        # # Recibe el nombre del archivo
-        # filename = connection.recv(1024).decode()
-        # print(f"Recibiendo archivo: {filename}")
-
-        # # Recibe el archivo
-        # receive_file(connection, filename)
-        # print(f"Archivo {filename} recibido correctamente")
-
-        # # Cierra la conexión
-        # connection.close()
-
 cloud_server()
-
-# VERSION 1
-
-# import socket
-
-# def cloud_server():
-#     # Crear un socket TCP/IP
-#     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-#     # Asigna el socket al puerto 8000
-#     sock.bind(("192.168.3.6", 8000))
-
-#     # Escucha conexiones entrantes
-#     sock.listen(1)
-
-#     while True:
-#          # Acepta una conexión
-#         connection, address = sock.accept()
-
-#         #Lee los datos del cliente
-#         data = connection.recv(1024)
-
-#         # Imprime los datos recibidos
-#         print(data)
-
-# cloud_server()
